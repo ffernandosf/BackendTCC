cb0313d3e78ad8f50738a6544fbf526e29d04486
Fernando <ferreirasteam@gmail.com>
2025-06-06 10:32:10 -0300

    Termina configuracoes settings.py

--- projetoBack/core/settings.py
+++ projetoBack/core/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -124,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ORGINS = [
+    'http://localhost:8080']
